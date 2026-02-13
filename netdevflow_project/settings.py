b23fc5ffef591e80e8013ca73f9f3a386b84425b
Fran <fajterry@gmail.com>
2026-02-13 00:03:58 +0000

    Enhance admin UI with custom styles and improve footer branding; update settings for login/logout redirects

--- netdevflow_project/settings.py
+++ netdevflow_project/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inventory',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_REDIRECT_URL = 'login'
